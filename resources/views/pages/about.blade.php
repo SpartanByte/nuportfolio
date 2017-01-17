e6d74afc9e0e0f09f6534d3cc5ee876a44ac8450
Brian Wardwell <bwardwell@greatinsight.com>
2017-01-17 15:55:38 -0600

    Updated CSS and 'About'

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,33 +4,28 @@
     <div class="static-content">
         <h1>A Brief Story About Myself</h1>
 
-        <!-- start of background/bio -->
+        {{-- start of background/bio --}}
         <h3>Background</h3>
 
-            <p>I am a native of Fargo, North Dakota and attended Fargo North High School. I chose Web Development as a career path for many reason. I'm really captivated by the vastly different roles and hats as well as the different tools, technologies, environments and the list goes on. I have a vigourous creative side, enjoying Adobe Photoshop, Photography, vector graphics, User Interface/User Experience (UI/UX) Design.
+            <p>I am a native of Fargo, North Dakota and attended Fargo North High School. Years later, I enrolled in the <em>Web Development AAS</em> program at <a href="#" alt="Minnesota State Community and Technical College" target="_self">Minnesota State Community and Technical College</a> to which I graduated from in the summer of 2015 with additional credits. Course areas included HTML/CSS, Javascript, Web Applications Programming, PHP/MySQL, Python (from the Computer Programming courses), Content Management Systems, and User Interface/Experience Design. However, my passion to learn more and dig deeper didn't end there.</p>
+
+            <p> I am currently enrolled in the <em>Internet Programming</em> certificate program at <a href="#">North Hennepin Community College</a> which is focusing on Object Oriented Programming, Structured Computer Programming, Software Engineering and Database Design. I loved learning more Java and C# and am looking forward to adding ASP.NET to my skillset. I had also taken Java, C++/C# and SQL during a semester at <a href="#">Minnesota State College - Southeast Technical</a> in the spring of 2016.</p>
 
 
-        <!-- start of skill areas -->
-        {{-- <h3>Front-End</h3>
-        <hr />
-            <p>I'm fascinated by the ever changing trends and standards in layouts and I'm always looking for the "WOW" factor. I love CSS transitions, animations, and many more of the tools that are growing and expanding for stylesheets. </p>
+            <p>Professionally, I began a temporary contract with <a href="#">Dakota Supply Group</a> merging two internal websites into one cohesive Wordpress site. When this concluded in October, I joined <a href="#">Insight Technologies</a>as a full time Web Developer. It has been a wonderful experience already and introduced me to PHP's <a href="#">Laravel</a> MVC framework among other technologies.</p>
 
-        <h3>Back-End</h3>
-        <hr />
-            <p>A more logical side that's fascinated with functionality and solutions has magnetized itself on the backend as well.</p>
-        --}}
+
 
         <hr />
-        <h3>Skill-Set</h3>
-
+        <h4>Skill-Set</h4>
             <ul>
-                <li>Front-End</li>
+                <li><strong>Front-End</strong></li>
                     <ul>
                         <li>CSS / Sass</li>
                         <li>JavaScript</li>
                         <li>Adobe Photoshop</li>
                     </ul>
-                <li>Back-End</li>
+                <li><strong>Back-End</strong></li>
                     <ul>
                         <li>Laravel (PHP Framework)</li>
                         <li>PHP</li>
@@ -39,25 +34,22 @@
                     </ul>
             </ul>
 
-        <!-- Start of Academics -->
-        <h3>Academics:</h3>
-        <hr />
-            <h4>Minnesota State Community and Technical College, Web Development AAS</h4>
-                <p><strong>Graduated: May 2015, Moorhead MN</strong></p>
-            <h4>Minnesota State College - Southeast Technical, Web Programming</h4>
-                <p><strong>Attended 2016, eCampus</strong></p>
-            <h4>North Hennepin Community College, Internet and Object-Oriented Programming</h4>
-                <p><strong>Attended 2016-??, eCampus</strong></p>
-
-        <!-- Start of Professional/Personal Development -->
-        <h3>Professional and Personal Development:</h3>
-        <hr />
-            <h4><a href="http://musicbytes.us/" title="MusicBytes: Entertainment" alt="MusicBytes.us: Entertainment Website">MusicBytes.us</a></h4>
-                <p><strong>Music Entertainment Website, Web Developer (2012 - Current)</strong></p>
-            <h4>Dakota Supply Group</h4>
-                <p><strong>Intranet/Internal Website (Temp/Contract), June 2016-Current (Maintenance Contract)</strong></p>
-            <h4>Insight Technologies</h4>
+        {{-- Start of Professional/Personal Development --}}
+        <h4>Professional and Personal Development:</h4>
+            <h5>Insight Technologies</h5>
                 <p><strong>Web Developer, October 2016 - Current</strong></p>
+            <h5>Dakota Supply Group</h5>
+                <p><strong>Intranet/Internal Website (Temp/Contract), June 2016-Current (Maintenance Contract)</strong></p>
+            <h5><a href="http://musicbytes.us/" title="MusicBytes: Entertainment" alt="MusicBytes.us: Entertainment Website">MusicBytes.us</a></h5>
+                <p><strong>Music Entertainment Website, Web Developer (2012 - Current)</strong></p>
 
+
+            <h4>Academics:</h4>
+                <h5>North Hennepin Community College, Internet and Object-Oriented Programming</h5>
+                    <p>Attended 2016-??, eCampus</p>
+                <h5>Minnesota State College - Southeast Technical, Web Programming</h5>
+                    <p>Attended Spring 2016, eCampus</p>
+                <h5>Minnesota State Community and Technical College, Web Development AAS</h5>
+                    <p><strong>Graduated: May 2015, Moorhead MN</strong></p>
     </div>
 @stop
