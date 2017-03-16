902f2cb31af32fe39291bc58005a3a899c78ac42
Brian Wardwell <bwardwell@greatinsight.com>
2017-03-16 16:44:43 -0500

    cleaning up CSS

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
 
 <!-- loading boostrap from CDN -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> --}}
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 {{-- jQuery links  --}}
