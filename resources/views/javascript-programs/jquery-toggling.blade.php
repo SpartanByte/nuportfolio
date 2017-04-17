244fcc92276b59fdcf4cec61937c118833871010
Brian Wardwell <bwardwell@greatinsight.com>
2017-04-17 16:42:36 -0500

    initial javascript work

--- resources/views/javascript-programs/jquery-toggling.blade.php
+++ resources/views/javascript-programs/jquery-toggling.blade.php
@@ -6,13 +6,14 @@
     <div class="static-content program-page">
 
         {{-- Current CSS isn't showing anything above the following H1, will need to fix this  --}}
-        <h1 class="page-header">Toggling and Other Visual Effects with jQuery</h1>
+        <h1 class="page-header extended-header">Toggling and Other Visual Effects with jQuery</h1>
         <hr />
 
         <img src="/images/logos/jquery-logo.png" />
-        <h3>Very Simple Toggling with jQuery</h3>
-        <p>For additional information, <a href="outgoingLink" title="CSS Tricks">CSS Tricks</a> is a wonderful resource as well as training/tutorial hub for beginner developers.</p>
-
+        <div class="program-description">
+            <h3 class="program-header">Very Simple Toggling with jQuery</h3>
+            <p>For additional information, <a href="outgoingLink" title="CSS Tricks">CSS Tricks</a> is a wonderful resource as well as training/tutorial hub for beginner developers.</p>
+        </div>
         <hr />
 
    
