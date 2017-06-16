9d6a0a16aa9871900f34b9b62955dd395d5b7b8a
Brian Wardwell <bwardwell@greatinsight.com>
2017-06-16 12:08:03 -0500

    adding programming language content, c# files

--- resources/views/coding/csharp.blade.php
+++ resources/views/coding/csharp.blade.php
@@ -8,9 +8,21 @@
         <span class="page-header uppercase-shadow">C# Code Samples</span>
         <hr />
 
-        <img src="/images/logos/csharp.png" />
+        <img src="/images/logos/csharp.png" width="200" />
         <h3>C# Code Samples</h3>
         <p>For additional information, visit<a class="outgoingLink" href="http://www.c-sharpcorner.com/"> C# Corner</a>, a social and support community for C# developers and programmers.</a></p>
-        <hr />
+        <blockquote><strong>Note:</strong> Due to working a solution to convert the current C# programs, Windows Forms Applications, to ASP.NET and considering using a Connected System to show them,
+                major code portions/examples are in .pdf format. Also listed in a downloadable zip file which includes the solution but not every detail, for security reasons.</blockquote>
+
+            <p class="mid-size-font uppercase-shadow">C# Samples</p>
+            <ul class="file-list med-size-font">
+                    <li><a href="/files/csharp/AveragesForm-CSharp.pdf">Averages Form: 'Form1.cs'</a><img src="/icons/file-icons/pdf-file.png"/> 
+                    <a href="/files/csharp/AveragesForm.zip">AveragesForm.zip</a><img src="/icons/file-icons/zip-file.png"/></li>
+                    <li><a href="/files/csharp/GuessAWord.pdf">GuessAWord: 'GuessAWord.cs' (Web Form)</a><img src="/icons/file-icons/pdf-file.png"/></li>
+                    <li><a href="/files/csharp/AveragesForm.zip">GuessAWord.zip</a><img src="/icons/file-icons/zip-file.png"/></li>
+                     <li><a href="/files/csharp/GuessAWord.pdf">PrimeFactors: 'PrimeForm.cs' (Web Form)</a><img src="/icons/file-icons/pdf-file.png"/></li>
+                    <li><a href="/files/csharp/AveragesForm.zip">Prime Factors.zip</a><img src="/icons/file-icons/zip-file.png"/></li>
+            </ul>
+
     </div>
 @stop
