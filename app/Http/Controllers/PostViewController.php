f1659bc4c9a525d410ff4874f4dad1df0ec9e720
Brian Wardwell <bwardwell@greatinsight.com>
2017-07-03 14:34:32 -0500

    Separated front end view and admin views for post and management

---
+++ app/Http/Controllers/PostViewController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
+
+class PostViewController extends Controller
+{
+
+    // Show list of posts with brief descriptions for users
+    public function index()
+    {
+        
+            $posts = Post::all();
+            return view ('posts.index')->with(['posts'=>$posts]);
+    }
+
+    // Show individual post
+    public function show($id)
+    {
+            $post = Post::find($id);
+            return view('posts.show')->with('post', $post);
+    }
+}
