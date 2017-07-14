f2def2e9aedfb4e20ff263ffd24eac2813ee9145
Brian Wardwell <bwardwell@greatinsight.com>
2017-07-14 16:17:36 -0500

    Changed homepage layout, adjusted CSS styles and footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,19 +4,30 @@
 
 @section('content')
     <div class="home-pad">
-        <div class="landing-image">
-            <img src="/images/stock/laptop_time.jpg" class="img-100" />
-        </div>
 
+        <h1>Brian Wardwell</h1>
+        <h2>Web Developer | Musician | Amateur Photographer | Writer</h2>
+        {{--<p><img src="/images/stock/code-screen.jpg" class=""/></p>--}}
+        <img src="/images/stock/laptop_time.jpg" class="" />
+        <h3>Web Development focusing on innovation, aesthetics, user experience and custom functionality</h3>
+        <p>Sole musician in musical project, <a href="https://soundcloud.com/battle-cry-wardwell" target="_blank">Battle Cry</a> (Recorded from 1997-2012) | Vocals, Guitar, Bass, Keyboards <br />
+            Sole administrator and writer for entertainment website, <a href="http://musicbytes.us/" target="_blank">MusicBytes</a> (launched in January 2015) <br />
+            Also an amateur photographer and graphics manipulator</p>
+
+        <div class="landing-image">
+            {{--<img src="/images/stock/code-screen.jpg" class=""/>--}}
+        </div>
         <div id="right-home">
                 <section>
-                    <h1>Brian Wardwell</h1>
-                    <h2>Web Developer | Musician | Amateur Photographer | Writer</h2>
-                    <h3>Web Development focusing on innovation, aesthetics, user experience and custom functionality</h3>
-                    <p>Sole musician in musical project, <a href="https://soundcloud.com/battle-cry-wardwell" target="_blank">Battle Cry</a> (Recorded from 1997-2012) | Vocals, Guitar, Bass, Keyboards <br />
-                    Sole administrator and writer for entertainment website, <a href="http://musicbytes.us/" target="_blank">MusicBytes</a> (launched in January 2015) <br />
-                    Also an amateur photographer and graphics manipulator</p>
-                    <p><img src="/images/stock/code-screen.jpg" class="img-100"/></p>
+                    <img src="/images/stock/code-screen.jpg" class="img-100"/>
+                    {{--<h1>Brian Wardwell</h1>--}}
+                    {{--<h2>Web Developer | Musician | Amateur Photographer | Writer</h2>--}}
+                    {{--<p><img src="/images/stock/code-screen.jpg" class="img-100"/></p>--}}
+                    {{--<h3>Web Development focusing on innovation, aesthetics, user experience and custom functionality</h3>--}}
+                    {{--<p>Sole musician in musical project, <a href="https://soundcloud.com/battle-cry-wardwell" target="_blank">Battle Cry</a> (Recorded from 1997-2012) | Vocals, Guitar, Bass, Keyboards <br />--}}
+                    {{--Sole administrator and writer for entertainment website, <a href="http://musicbytes.us/" target="_blank">MusicBytes</a> (launched in January 2015) <br />--}}
+                    {{--Also an amateur photographer and graphics manipulator</p>--}}
+                    {{--<p><img src="/images/stock/code-screen.jpg" class="img-100"/></p>--}}
                 </section>
         </div>
 </div>
