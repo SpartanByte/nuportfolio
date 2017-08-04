67cfd18a8a8184d50d682d71e0898cea1a38a5bd
Brian Wardwell <bwardwell@greatinsight.com>
2017-08-04 12:43:22 -0500

    Filled in content on javascript.blade.php and php.blade.php

--- resources/views/coding/php.blade.php
+++ resources/views/coding/php.blade.php
@@ -13,18 +13,27 @@
 
         <img src="/images/logos/php.png" class="img-20"/>
        <span class="section-header mid-size-font">PHP</span>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor rhoncus ultrices. Phasellus a tortor quis erat iaculis cursus. Vivamus sapien tortor, ultrices in ex in, vulputate placerat ante. Pellentesque a tempus enim, eget ultricies leo. Proin vitae convallis mauris, sit amet hendrerit nibh. Donec in tincidunt lacus. Nullam eu commodo diam. Quisque convallis, augue ut rhoncus porta, lectus ligula tempor nisl, nec imperdiet lorem nulla ut purus. Vestibulum turpis elit, dignissim sit amet congue eu, molestie ac justo.</p>
-
-            <p>Nullam egestas ullamcorper lorem, ut blandit tellus eleifend vitae. Aenean erat arcu, pharetra vel eros ornare, consectetur sollicitudin nibh. Donec quis mauris ut elit ornare dignissim. Nam sagittis iaculis purus sit amet cursus. Aliquam enim nisi, efficitur vel dui a, lacinia ornare lacus. Donec sed consectetur sapien, sed vestibulum lectus. Vivamus sagittis dui mauris, vel finibus ipsum pretium non. Phasellus fermentum bibendum lacus, sed volutpat massa scelerisque quis. Donec rutrum euismod ultrices. In ac tincidunt felis. Donec non ex felis.</p>
-
-            <p>In congue nulla eget laoreet euismod. Nunc mattis est sapien, vel scelerisque neque molestie eu. Donec convallis placerat justo, nec mollis erat pharetra quis. Nam tempus erat ut quam dignissim viverra. Duis semper mauris ac porta feugiat. In non tortor velit. Nulla vel dui auctor tellus consectetur ultrices quis vitae sem. Suspendisse egestas, risus at consequat molestie, mi elit egestas lacus, in dignissim dui libero porta orci. </p>
+            <p>For aspiring web developers, PHP is often the first true server-side programming language that they work with, although this is not always the case. One of the reasons is the versatility of PHP in web environments because it was developed for web application programming. Therefore, even when writing in procedural PHP, it interacts with HTML and JavaScript very easily. Another reason is that the syntax if fairly easy to understand when beginning to learn, at least in procedural programming. Many programming practices, however, need to be implemented to ensure that secure PHP is being written rather than having many vulnerabilities such as SQL Injection.</p>
 
             <p><a href="#">Concert Ticket Ordering Program</a></p>
             <p><a href="#">Business Travel Expenses Calculator</a></p>
             <p><a href="#">Airline Arrival Program</a></p>
-            {{-- additional projects will continue like this. -- will be easy to advance into a model returning a request for files related to PHP samples --}}
+            {{-- additional projects will continue like this. will be easy to advance into a model returning a request for files related to PHP samples --}}
 
+            <div><br /><span class="page-header mid-size-font">PHP FRAMEWORKS</span><br /></div>
+            <p>As with most, if not all, programming languages, there are many frameworks available for PHP and each seem to have their own unique pros and cons. Also, as technology moves forward, server-side frameworks in particular sometimes become entirely outdated due to new security practices, new programming paradigms and other reasons. </p>
+
+              <img src="/images/logos/laravel-rectangle.jpg"  width="400"/>
+            <div><br /><span class="page-header mid-size-font">Laravel</span><br /></div>
+            <p><a href="https://laravel.com" title="Laravel Documentation" target="_blank">Laravel</a> is an MVC PHP Framework that has certainly been creating a lot of attention and generating popularity. It was created in 2011 by <a href="https://github.com/taylorotwell" title="Taylor Otwell: GitHub">Taylor Otwell</a> as he wanted to provide a more advanced, flexible alternative to <a href="https://codeigniter.com/" title="Codeigniter" target="_blank">Codeigniter</a> (another PHP Framework)(<a href="https://en.wikipedia.org/wiki/Laravel" title="Laravel Wikipedia" target="_blank">Source</a>), specifically to have built in user authentication and authorization capabilities. With Laravel 2 (in late 2011), the template language <strong>Blade</strong> was added for Laravel views to give a simpler, cleaner syntax that is both minimal and easy to understand. Fastforwarding, Laravel 5.4 was released in January of 2017, with 5.5 potentially coming by the end of 2017. Components such as <strong>Artisan</strong> (Laravel's command line), <strong>Eloquent</strong> (Object Relational Mapping for Databases), <strong>Elixir</strong> (an API for managing tasks for pre-processors), and built in validation rules and now coming with Vue.js out of the box makes Laravel a very powerful choice. This is strengthened when considering its flexibility such as allowing developers to create and assign their own Service, Worker, Job and other types of classes.</p>
+            <p>Many trends are showing that most PHP Frameworks are fading out, at least at this point, other than Laravel. Sitepoint published <a href="https://www.sitepoint.com/the-state-of-php-mvc-frameworks-in-2017/" title="The State of PHP MVC Frameworks in 2017" target="_blank">"The State of PHP MVC Frameworks in 2017"</a> stating that trends for previously popular frameworks such as <strong>Zen</strong>, <strong>CakePHP</strong>, <strong>CodeIgniter</strong> have all but phased out. The article still includes <a href="https://symfony.com/" title="Symfony PHP Framework" target="_blank">Symfony</a> in the running as Symfony 3 was still yet to be released (Symfony was released in August, 2017).</p>
+
+
+            <img src="/images/logos/symfony.png"  width="400"/>
+            <div><br /><span class="page-header mid-size-font">Symfony</span><br /></div>
         <hr />
+
+
 
    
     </div>
