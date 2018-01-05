478b0d4c828282855dfe04f245d4e4d6b2285db9
Brian Wardwell <briwar10@outlook.com>
2018-01-05 15:01:04 -0600

    updated homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,17 +5,19 @@
     <div class="home-pad">
         <h1>Brian Wardwell</h1>
         <h2>Web Developer | Musician | Amateur Photographer | Writer</h2>
-        <img src="/images/stock/laptop_time.jpg" class="img-50" />
-        <h3>Web Development focusing on innovation, aesthetics, user experience and custom functionality</h3>
-        <p>Sole musician in musical project, <a href="https://soundcloud.com/battle-cry-wardwell" target="_blank">Battle Cry</a> (Recorded from 1997-2012) | Vocals, Guitar, Bass, Keyboards <br />
-            Sole administrator and writer for entertainment website,
-            <a href="http://musicbytes.us/" target="_blank">MusicBytes</a> (launched in January 2015) <br />
-            Also an amateur photographer and graphics manipulator</p>
-            <div class="landing-image">
+            <div class="left-home">
+                <img src="/images/stock/laptop_time.jpg" class="img-50" />
+                <h3>Web Development focusing on innovation, aesthetics, user experience and custom functionality</h3>
+                <p>Sole musician in musical project, <a href="https://soundcloud.com/battle-cry-wardwell" target="_blank">Battle Cry</a> (Recorded from 1997-2012) | Vocals, Guitar, Bass, Keyboards <br />
+                    Sole administrator and writer for entertainment website,
+                    <a href="http://musicbytes.us/" target="_blank">MusicBytes</a> (launched in January 2015) <br />
+                    Also an amateur photographer and graphics manipulator
+                </p>
+                <div class="landing-image"></div>
             </div>
             <div id="right-home">
             <section>
-                <img src="/images/stock/php-code.jpg" class="img-100"/><br />
+                <img src="/images/stock/php-screen.jpg" class="img-100"/><br />
                 <caption>Today is: {{$today}}</caption>
             </section>
         </div>
