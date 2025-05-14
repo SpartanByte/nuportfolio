e48d6c9f5f4c61e47b78f85e712d786edac548b2
Brian Wardwell <briwar10@outlook.com>
2025-05-14 12:49:18 -0500

    update information on homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,12 +4,14 @@
 @section('content')
     <div class="home-pad">
         <h1>Brian Wardwell &#10072; Web Developer/Software Engineer</h1>
-       {{--  <caption>Today is: {{$today}}</caption> --}}
             <div class="home-container">
                 <img src="/images/site-images/sublime-code.png">
-                <h3>Systematic web and software developer with experience creating and maintaining existing web applications using a variety of technologies.</h3>
+                <p class="uppercase-shadow mid-size-font">Today is: {{$today}}</caption>
+                <h2>Versatile web and software developer with 7+ years of professional experience, musician, amateur photographer, dog owner, and lover of Minnesota's state parks.</h2>
                 <blockquote>
-                	<!--<em>This site is built using Laravel PHP framework. Visit my previous portfolio below, built in generic PHP.</em>-->
+                    <p>
+                        Hello, welcome to my web portfolio. Please be aware that you are currently viewing an version of the site that is older in respect to visual design and was developed with an earlier iteration of Laravel. I am actively engaged in an exciting redesign and development process, crafting a brand-new portfolio as a dynamic single-page application using Next.js, Tailwind CSS, and the animation capabilities of Framer Motion. Prepare for an enhanced and engaging showcase of my work – stay tuned for the unveiling.
+                    </p>
                 </blockquote>
                 <button type="button" class="btn btn-primary my-blue-btn"><a href="http://portfolio.brianwardwell.com/pages/experience" title="Professional Experience">Professional Experience</button>
             </div>
