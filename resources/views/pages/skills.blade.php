d7daf871f9f3afcffbd17ff71c8388a2aa5abad3
Brian Wardwell <briwar10@outlook.com>
2025-05-19 15:05:39 -0500

    update css for new mobile menu

---
+++ resources/views/pages/skills.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.default')
+@section('description', 'About Brian Wardwell: web developer, musician, amateur photographer, writer, native of Fargo North Dakota')
+@section('title', 'My Background')
+
+@section('content')
+<div class="static-content skills">
+    <h1 style="text-align: left;">Skills (A-Z)</h1>
+
+    <h2 style="width: 50vw;" class="mid-size-font">Welcome to my master list of language used, software and tools, cloud services and other skills that I have used. It also includes soft skills that I am strong in. The goal of these lists is to enable more detail without cluttering up a resume.</h2>
+
+    <img style="width: 50vw;" src="/images/skills-images/programming-languages-header-crop-min.png">
+    <details>
+        <summary class="uppercase-shadow mid-size-font">Programming/Markup</summary>
+        <ul>
+            <li>Bootstrap</li>
+            <li>Capybara/RSpec (Ruby testing frameworks)</li>
+            <li>Content Management Systems (CMS)</li>
+            <li>CSS/SCCS</li>
+            <li>DynamicWeb (C# CMS)</li>
+            <li>HTML5</li>
+            <li>Java</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Laravel</li>
+            <li>MariaDB</li>
+            <li>MS SQL</li>
+            <li>MySQL</li>
+            <li>Next.js</li>
+            <li>PHP</li>
+            <li>PostreSQL</li>
+            <li>React.js</li>
+            <li>Ruby on Rails</li>
+            <li>Umbraco (C# CMS)</li>
+            <li>Vue.js</li>
+            <li>WordPress</li>
+
+        </ul>
+    </details>
+
+    <details>
+        <summary class="uppercase-shadow mid-size-font">Tools</summary>
+        <ul>
+            <li>Adobe Photoshop</li>
+            <li>Android Studio</li>
+            <li>Apache HTTP Server</li>
+            <li>Bitbucket/JIRA</li>
+            <li>Figma</li>
+            <li>Filezilla (SFTP)</li>
+            <li>Insomnia API Platform</li>
+            <li>Laravel Herd</li>
+            <li>Microsoft Office</li>
+            <li>Microsoft Visio</li>
+            <li>Microsoft SSMS (SQL Server Management Studio)</li>
+            <li>Microsoft Visual Studio</li>
+            <li>Microsoft VS Code</li>
+            <li>Nginx</li>
+            <li>Postman API Platform</li>
+            <li>Sequel Ace (local databases)</li>
+            <li>Sublime Text</li>
+        </ul>
+    </details>
+
+    <details>
+        <summary class="uppercase-shadow mid-size-font">Cloud Services</summary>
+        <ul>
+            <li>AWS S3</li>
+            <li>Cloudflare</li>
+            <li>Github</li>
+            <li>GitLab</li>
+            <li>Heroku</li>
+            <li>Laravel Forge</li>
+            <li>Runcloud</li>
+            <li>Visual Studio Team Services (VSTS, now Azure)</li>
+        </ul>
+    </details>
+
+    <details>
+        <summary class="uppercase-shadow mid-size-font">Other</summary>
+        <ul>
+            <li>Agile Scrum</li>
+            <li>Augmented Reality (AR)</li>
+            <li>CI/CD Deployment</li>
+            <li>Code Reviews/Pull Requests</li>
+            <li>DNS/SSL Certificate Management</li>
+            <li>Homebrew</li>
+            <li>IoT (Internet of Things)</li>
+            <li>Mac OS Development Environments (local)</li>
+            <li>Responsive Design</li>
+            <li>SEO (Search Engine Optimization)</li>
+            <li>Thingworx</li>
+            <li>Unit Testing</li>
+            <li>UX/UI Design</li>
+            <li>Vuforia</li>
+            <li>Windows Development Environments (local)</li>
+        </ul>
+    </details>
+</div>
+@stop
