12c146c3b284664794db6ae9f8be3506045a1ec6
Brian Wardwell <briwar10@outlook.com>
2025-07-15 02:41:54 -0500

    update with home message announcing new site

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,13 +6,16 @@
         <h1>Brian Wardwell &#10072; Web Developer and Software Developer</h1>
             <div class="home-container">
                 <blockquote class="home-notice">
+                    <button type="button" class="btn btn-primary my-blue-btn">
+                        <a href="http://developer.brianwardwell.com" title="NextJS Web Developer Portfolio">Visit my NEW Portfolio</a>
+                    </button>
+                    <p>Note: I will be updating redirection once the SSL certificate is installed on the new site.</p>
+                </blockquote>
+                <blockquote class="home-notice">
                     <button type="button" class="btn btn-primary my-blue-btn"><a href="{{ route('background') }}" title="Background">Background</a></button>
                     <button type="button" class="btn btn-primary my-blue-btn"><a href="{{ route('experience') }}" title="Professional Experience">Experience</a></button>
                     <button type="button" class="btn btn-primary my-blue-btn"><a href="{{ route('skills') }}" title="Skills List">Skills List</a></button>
                     <p class="uppercase-shadow mid-size-font">Today is: {{$today}}</caption>
-                    <p>
-                        Hello, welcome to my web portfolio. Please be aware that you are currently viewing an version of the site that is older in respect to visual design and was developed with an earlier iteration of Laravel. I am actively engaged in an exciting redesign and development process, crafting a brand-new portfolio as a dynamic single-page application using Next.js, Tailwind CSS, and the animation capabilities of Framer Motion. Prepare for an enhanced and engaging showcase of my work – stay tuned for the unveiling.
-                    </p>
                 </blockquote>
                 <img src="/images/site-images/sublime-code.png">
             </div>
